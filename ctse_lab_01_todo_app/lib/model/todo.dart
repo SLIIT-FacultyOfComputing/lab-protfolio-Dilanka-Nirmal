879fa18d63de7388585eea7aaf332cfb0ae4e063
Dilanka Nirmal <dilankanirmal138@gmail.com>
2023-02-16 19:26:10 +0530

    To-Do model created

---
+++ ctse_lab_01_todo_app/lib/model/todo.dart
@@ -0,0 +1,34 @@
+class ToDo {
+  String? id;
+  String? todoText;
+  bool isDone;
+
+  ToDo({
+    required this.id,
+    required this.todoText,
+    this.isDone = false,
+  });
+
+  static List<ToDo> todoList() {
+    return [
+      ToDo(id: '01', todoText: 'CTSE Lectures', isDone: true),
+      ToDo(id: '02', todoText: 'CTSE Labs', isDone: true),
+      ToDo(
+        id: '03',
+        todoText: 'CTSE Tutorial',
+      ),
+      ToDo(
+        id: '04',
+        todoText: 'IUP Lectures',
+      ),
+      ToDo(
+        id: '05',
+        todoText: 'IUP Labs',
+      ),
+      ToDo(
+        id: '06',
+        todoText: 'Meeting with the Supervisor',
+      ),
+    ];
+  }
+}
