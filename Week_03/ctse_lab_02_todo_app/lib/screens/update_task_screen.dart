6b23abf6bd2c6d15f50ca2464f4dbea5e7c85c14
Dilanka Nirmal <dilankanirmal138@gmail.com>
2023-02-27 02:34:26 +0530

    Update task screen created

---
+++ Week_03/ctse_lab_02_todo_app/lib/screens/update_task_screen.dart
@@ -0,0 +1,72 @@
+// ignore_for_file: library_private_types_in_public_api
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:ctse_lab_02_todo_app/models/task_model.dart';
+
+class UpdateTaskScreen extends StatefulWidget {
+  final TaskModel task;
+
+  const UpdateTaskScreen({Key? key, required this.task}) : super(key: key);
+
+  @override
+  _UpdateTaskScreenState createState() => _UpdateTaskScreenState();
+}
+
+class _UpdateTaskScreenState extends State<UpdateTaskScreen> {
+  var nameController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    nameController.text = widget.task.taskName;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Update Task'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: nameController,
+              decoration: const InputDecoration(hintText: 'Task name'),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            ElevatedButton(
+                onPressed: () async {
+                  var taskName = nameController.text.trim();
+                  if (taskName.isEmpty) {
+                    Fluttertoast.showToast(msg: 'Please provide task name');
+                    return;
+                  }
+
+                  User? user = FirebaseAuth.instance.currentUser;
+
+                  if (user != null) {
+                    DatabaseReference taskRef = FirebaseDatabase.instance
+                        .ref()
+                        .child('tasks')
+                        .child(user.uid)
+                        .child(widget.task.taskId);
+
+                    await taskRef.update({
+                      'taskName': taskName,
+                    });
+                  }
+                },
+                child: const Text('Update')),
+          ],
+        ),
+      ),
+    );
+  }
+}
